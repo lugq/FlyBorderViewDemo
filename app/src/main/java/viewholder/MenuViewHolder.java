4bf04237e383147d7738414d0f81a2ef7690e454
LuGuoqiang <luguoqiang_vendor@sensetime.com>
2019-04-30 11:29:34 +0800

    缩放比抽取

--- app/src/main/java/viewholder/MenuViewHolder.java
+++ app/src/main/java/viewholder/MenuViewHolder.java
@@ -20,6 +20,14 @@ public class MenuViewHolder extends BaseViewHolder<Movie> {
     private TextView title;
     private TextView content;
 
+    // 有焦点时候得缩放比 1.20f
+    private static float FOCUS_SCALEX = 1.0f;
+    private static float FOCUS_SCALEY = 1.0f;
+
+    // 正常情况缩放比
+    private static float NORMAL_SCALEX = 1.0f;
+    private static float NORMAL_SCALEY = 1.0f;
+
     public MenuViewHolder(View itemView) {
         super(itemView);
         title= (TextView) itemView.findViewById(R.id.Menu_Title);
@@ -55,8 +63,8 @@ public class MenuViewHolder extends BaseViewHolder<Movie> {
             if(Build.VERSION.SDK_INT>=21){
 //                ViewCompat.animate(itemView).scaleX(1.20f).scaleY(1.20f).translationZ(1).start();
 
-                ObjectAnimator scaleX=ObjectAnimator.ofFloat(itemView,"scaleX",1.0f,1.20f);
-                ObjectAnimator scaleY=ObjectAnimator.ofFloat(itemView,"scaleY",1.0f,1.20f);
+                ObjectAnimator scaleX=ObjectAnimator.ofFloat(itemView,"scaleX",NORMAL_SCALEX,FOCUS_SCALEX);
+                ObjectAnimator scaleY=ObjectAnimator.ofFloat(itemView,"scaleY",NORMAL_SCALEY, FOCUS_SCALEY);
                 ObjectAnimator translationZ=ObjectAnimator.ofFloat(itemView,"translationZ",0f,1.0f);
                 AnimatorSet animatorSet=new AnimatorSet();
                 animatorSet.play(scaleX).with(scaleY).with(translationZ);
@@ -64,8 +72,8 @@ public class MenuViewHolder extends BaseViewHolder<Movie> {
                 animatorSet.start();
             }else{
 //                ViewCompat.animate(itemView).scaleX(1.20f).scaleY(1.20f).start();
-                ObjectAnimator scaleX=ObjectAnimator.ofFloat(itemView,"scaleX",1.0f,1.20f);
-                ObjectAnimator scaleY=ObjectAnimator.ofFloat(itemView,"scaleY",1.0f,1.20f);
+                ObjectAnimator scaleX=ObjectAnimator.ofFloat(itemView,"scaleX",NORMAL_SCALEX,FOCUS_SCALEX);
+                ObjectAnimator scaleY=ObjectAnimator.ofFloat(itemView,"scaleY",NORMAL_SCALEY,FOCUS_SCALEY);
                 ObjectAnimator translationZ=ObjectAnimator.ofFloat(itemView,"translationZ",0f,1.0f);
                 AnimatorSet animatorSet=new AnimatorSet();
                 animatorSet.play(scaleX).with(scaleY).with(translationZ);
@@ -84,8 +92,8 @@ public class MenuViewHolder extends BaseViewHolder<Movie> {
             if(Build.VERSION.SDK_INT>=21){
 //                ViewCompat.animate(itemView).scaleX(1.0f).scaleY(1.0f).translationZ(0f).start();
 
-                ObjectAnimator scaleX=ObjectAnimator.ofFloat(itemView,"scaleX",1.20f,1.0f);
-                ObjectAnimator scaleY=ObjectAnimator.ofFloat(itemView,"scaleY",1.20f,1.0f);
+                ObjectAnimator scaleX=ObjectAnimator.ofFloat(itemView,"scaleX",FOCUS_SCALEX,NORMAL_SCALEX);
+                ObjectAnimator scaleY=ObjectAnimator.ofFloat(itemView,"scaleY",FOCUS_SCALEY,NORMAL_SCALEY);
                 ObjectAnimator translationZ=ObjectAnimator.ofFloat(itemView,"translationZ",1.0f,0f);
                 AnimatorSet animatorSet=new AnimatorSet();
                 animatorSet.setDuration(200);
@@ -93,8 +101,8 @@ public class MenuViewHolder extends BaseViewHolder<Movie> {
                 animatorSet.start();
             }else{
 //                ViewCompat.animate(itemView).scaleX(1.0f).scaleY(1.0f).translationZ(0f).start();
-                ObjectAnimator scaleX=ObjectAnimator.ofFloat(itemView,"scaleX",1.20f,1.0f);
-                ObjectAnimator scaleY=ObjectAnimator.ofFloat(itemView,"scaleY",1.20f,1.0f);
+                ObjectAnimator scaleX=ObjectAnimator.ofFloat(itemView,"scaleX",FOCUS_SCALEX,NORMAL_SCALEX);
+                ObjectAnimator scaleY=ObjectAnimator.ofFloat(itemView,"scaleY",FOCUS_SCALEY,NORMAL_SCALEY);
                 ObjectAnimator translationZ=ObjectAnimator.ofFloat(itemView,"translationZ",1.0f,0f);
                 AnimatorSet animatorSet=new AnimatorSet();
                 animatorSet.setDuration(200);
